8d23721f17cadddfb3d410332efd1119e5fc481d
williamcruzgouvea <william.cruz.gouvea@gmail.com>
2020-03-01 14:56:06 -0300

    -Create App extending Application class and add it to manifest -Add buildConfigFields REST_SERVER and GITHUB_TOKEN to build.gradle -Add dependencies libraries to build.gradle -Add INTERNET permission -Add divider.xml resource -Add circular imageView to repo_list_item.xml -Add selectable background to click animation at repo_list_item

---
+++ app/src/main/java/br/com/flyingdutchman/arena_challenge/App.kt
@@ -0,0 +1,34 @@
+package br.com.flyingdutchman.arena_challenge
+
+import android.app.Application
+import br.com.flyingdutchman.arena_challenge.di.apiModule
+import br.com.flyingdutchman.arena_challenge.di.netModule
+import br.com.flyingdutchman.arena_challenge.di.repositoryModule
+import br.com.flyingdutchman.arena_challenge.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class App : Application() {
+    companion object {
+        lateinit var instance: App
+            private set
+
+        const val IO_SCHEDULER = "IO_SCHEDULER"
+        const val MAIN_SCHEDULER = "MAIN_SCHEDULER"
+        const val HEADER_KEY_AUTHORIZATION = "Authorization"
+        const val NAMED_INTERCEPTOR_HEADER = "NAMED_INTERCEPTOR_HEADER"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+
+        startKoin {
+            androidContext(this@App)
+            androidLogger(if (BuildConfig.DEBUG) Level.DEBUG else Level.INFO)
+            modules(listOf(viewModelModule,repositoryModule, netModule, apiModule))
+        }
+    }
+}
